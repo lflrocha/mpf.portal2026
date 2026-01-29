7f9734d252137c23c4d3bf25a3a628cd6a7a64b1
Luis Flávio Loreto da Rocha <luis.rocha@ebc.com.br>
2026-01-29 16:29:54 -0300

    Ajustes para publicação em homologação

---
+++ src/mpf/portal2026/browser/servicos_view.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from plone import api
+from Products.CMFCore.utils import getToolByName
+from Products.Five import BrowserView
+
+
+class ServicosView(BrowserView):
+    """View Serviços"""
+    pass
